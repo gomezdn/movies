bea8e04d3a29404a09d68cdc520938ab27f7457e
gomezdn <gomezdn@outlook.com>
2022-02-17 02:56:19 -0300

    initial commit, vite chakra react ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import  { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div id='app'>
+        
+      </div>
+    </ChakraProvider>
+  )
+}
+
+export default App
