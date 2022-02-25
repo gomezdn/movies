d1c0bd63b73ebaa4a93b31352c64e9597afe373a
gomezdn <gomezdn@outlook.com>
2022-02-25 08:49:36 -0300

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text, HStack, Link } from '@chakra-ui/react'
 
 export default function Footer() {
   return (
     <Flex as='footer'>
-      
+      <HStack color='gray' pb='2em' alignSelf='center' align='center' justify='space-around' spacing='5em'>
+        <Text>This product uses the TMDB API but is not endorsed or certified by TMDB.</Text>
+        <Text>Made by <Link color='orange.200' target='_blank' href='https://www.linkedin.com/in/gomezdn/'>gomezdn</Link></Text>
+      </HStack>
     </Flex>
   )
 }
