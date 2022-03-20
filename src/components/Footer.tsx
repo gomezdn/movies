ce8f615b9fe8a220f4d23aa8f2eb2ebaa542710e
gomezdn <gomezdn@outlook.com>
2022-03-20 06:01:34 -0300

    linked portfolio in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <Flex as='footer'>
       <HStack color='gray' paddingX={['1em','0']} pb='1em' alignSelf='center' align='center' justify='space-around' spacing={['1em', '5em']}>
         <Text>This product uses the TMDB API but is not endorsed or certified by TMDB.</Text>
-        <Text>Made by <Link color='orange.200' target='_blank' href='https://www.linkedin.com/in/gomezdn/'>gomezdn</Link></Text>
+        <Text>Made by <Link color='orange.200' target='_blank' href='https://gomezdn.github.io/portfolio/'>gomezdn</Link></Text>
       </HStack>
     </Flex>
   )
