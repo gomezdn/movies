ac34ba06bf7e60ecd8d9a008965957f8c1b6226d
gomezdn <gomezdn@outlook.com>
2022-08-09 22:42:56 -0300

    auth form validations

---
+++ src/validations/authFormValidations.ts
@@ -0,0 +1,42 @@
+import { FormikErrors, FormikValues } from 'formik';
+
+function validateUsername(
+  values: FormikValues,
+  errors: FormikErrors<FormikValues>
+) {
+  const username = values.username.trim();
+
+  if (!username) {
+    errors.username = 'Enter username';
+  } else if (username.length < 4 || username.length > 12) {
+    errors.username = 'Username must have between 4 and 12 characters';
+  }
+}
+
+function validateEmail(
+  values: FormikValues,
+  errors: FormikErrors<FormikValues>
+) {
+  const email = values.email.trim();
+
+  if (!email) {
+    errors.email = 'Enter email';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+    errors.email = 'Invalid email format';
+  }
+}
+
+function validatePassword(
+  values: FormikValues,
+  errors: FormikErrors<FormikValues>
+) {
+  const password = values.password.trim();
+
+  if (!password) {
+    errors.password = 'Enter password';
+  } else if (password.length < 8 || password.length > 20) {
+    errors.password = 'Password must have between 8 and 15 characters';
+  }
+}
+
+export { validateUsername, validateEmail, validatePassword };
