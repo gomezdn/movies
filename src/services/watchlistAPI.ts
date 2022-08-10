8a634e96bb40f6c5122f5188bd4d3266db9b170d
gomezdn <gomezdn@outlook.com>
2022-08-10 06:53:13 -0300

    register and login services

--- src/services/watchlistAPI.ts
+++ src/services/watchlistAPI.ts
@@ -1,31 +1,44 @@
 import axios from 'axios';
 import { FormikValues } from 'formik';
+import { MediaObject } from '../Types';
 const BASE_URL = import.meta.env.VITE_WATCHLIST_API_URL;
 const GET_WATCHLIST = import.meta.env.VITE_USER_WATCHLIST_ENDPOINT;
 const SIGNUP = import.meta.env.VITE_SIGNUP_ENDPOINT;
 const LOGIN = import.meta.env.VITE_LOGIN_ENDPOINT;
 
-async function getWatchlist(userEmail: string) {
-  const axiosRes = await axios.get(`${BASE_URL}${GET_WATCHLIST}/${userEmail}`);
+function getAuthHeader(token: string) {
+  return { Authorization: `Bearer ${token}` };
+}
 
-  return axiosRes.data;
+async function getWatchlist(token: string) {
+  const header = getAuthHeader(token);
+
+  try {
+    const axiosRes = await axios.get(`${BASE_URL}${GET_WATCHLIST}`, {
+      headers: header,
+    });
+
+    return axiosRes.data;
+  } catch (e: any) {
+    return { error: e.response.data.message };
+  }
 }
 
 async function register(userData: FormikValues) {
   try {
-    await axios.post(`${BASE_URL}${SIGNUP}`, userData);
-  } catch (e) {
-    console.log(e);
+    const axiosRes = await axios.post(`${BASE_URL}${SIGNUP}`, userData);
+    return { message: axiosRes.data.message };
+  } catch (e: any) {
+    return { error: e.response.data.message };
   }
 }
 
 async function login(userData: FormikValues) {
   try {
     const axiosRes = await axios.post(`${BASE_URL}${LOGIN}`, userData);
-
-    return axiosRes.data;
-  } catch (e) {
-    console.log(e);
+    return { auth: axiosRes.data };
+  } catch (e: any) {
+    return { error: e.response.data.message };
   }
 }
 
